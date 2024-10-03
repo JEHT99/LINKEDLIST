25210a28aa479fc99b65052be438a89fe0b1782c
Jeht99 <Jeht99@outlook.com>
2024-10-03 16:22:27 -0600

    [linkedList.h]: LinkedList class created

--- linkedList.h
+++ linkedList.h
@@ -19,7 +19,27 @@ Node::Node(int nodeId, string nodeData){
     data = nodeData;
     next = nullptr;
 }
-
+//********************************************************************************
 Node::~Node(){}
+//********************************************************************************
+//********************************************************************************
+class LinkedList{
+    private:
+        int counter;
+        Node *head;
+        Node *tail;
+    protected:
+        Node *searchNode(int);
+        bool isEmpty();
+        void clearList();
+    public:
+        LinkedList();
+        ~LinkedList();
+        bool addNew(string data);
+        bool addNextTo(int target, string data);
+        bool edit(int target, string data);
+        bool delete(int target);
+        void show();
+};
 //********************************************************************************
 //********************************************************************************
