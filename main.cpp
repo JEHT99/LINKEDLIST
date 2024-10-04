38071efab60581f0a356c87507d0aba45f4f7084
Jeht99 <Jeht99@outlook.com>
2024-10-03 23:12:51 -0600

    [main.cpp]: Class tested

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include "linkedList.h"
 using namespace std;
 
 int main(){
-    cout<<"Hello world";
-    return 0;
+    LinkedList *myList = new LinkedList();
+    /*myList->addNew("HELLO");
+    myList->addNew("HELLO2");
+    myList->addNew("HELLO3");
+    myList->addNextTo(1,"HELLO 4");
+    myList->show();
+    cout<<endl;
+    myList->edit(0,"NOW IS HELLO 5");
+    myList->show();*/
+
+    myList->addNew("1");
+    myList->addNew("2");
+    myList->addNew("3");
+    myList->erase(1);
+    myList->addNextTo(1,"4");
+    myList->addNew("HELLO2");
+    myList->addNew("HELLO3");
+    myList->addNextTo(1,"HELLO 4");
+    //myList->addNew("2");
+
+    LinkedList *myList2 = new LinkedList(*myList);
+    myList->show();
+    cout<<endl;
+    myList2->show();
+
 }
