8f001208d46cbc2544f46123a1716a877f9613af
Jeht99 <Jeht99@outlook.com>
2024-10-04 20:27:34 -0600

    [main.cpp]: Menu created

--- main.cpp
+++ main.cpp
@@ -1,27 +1,49 @@
 #include <iostream>
-#include <string>
+#include <cstdlib>
 #include "linkedList.h"
 using namespace std;
 
 int main(){
-/*LinkedList<int> myList;
-myList.addNew(10);
-myList.addNew(20);
-myList.addNew(30);
-myList.addNextTo(1,40);
-myList.show();
+    LinkedList<std::string> myList;
+    std::string option;
 
-cout<<endl;
-LinkedList<int> myList2 = LinkedList<int> (myList);
-myList2.show();*/
+    bool stat = true;
+    while(stat){
+        system("clear");
 
-/*LinkedList<string> myList;
-myList.addNew("HELLO");
-myList.show();*/
+        cout<<"1. Add new element"<<endl;
+        cout<<"2. Add next to element"<<endl;
+        cout<<"3. Edit element"<<endl;
+        cout<<"4. Erase element"<<endl;
+        cout<<"5. Show content"<<endl;
+        cout<<"6. Exit"<<endl;
+        
+        cout<<"\n>>";
+        cin>>option;
 
-LinkedList<float> myList;
-myList.addNew(40.25);
-myList.show();
+        switch(atoi(option.c_str())){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                stat = false;
+                break;
+            default:
+                cout<<"\nBad input"<<endl;
+                break;
+        }
 
+    cout<<"\nPress Enter to continue...";
+    cin.ignore();
+    cin.get();
+    }
+    
 return 0;
 }
