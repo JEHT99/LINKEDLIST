f2000a8e20eb8f2c3457bfce4868931d4aa7f12a
Jeht99 <Jeht99@outlook.com>
2024-10-05 12:18:51 -0600

    [main.cpp]: add function created

--- main.cpp
+++ main.cpp
@@ -2,27 +2,46 @@
 #include <cstdlib>
 #include "linkedList.h"
 using namespace std;
+//********************************************************************************
+//********************************************************************************
+void options(){
+    system("clear");
 
+    cout<<"1. Add new element"<<endl;
+    cout<<"2. Add next to element"<<endl;
+    cout<<"3. Edit element"<<endl;
+    cout<<"4. Erase element"<<endl;
+    cout<<"5. Show content"<<endl;
+    cout<<"6. Exit"<<endl;
+}
+//********************************************************************************
+bool add(LinkedList<std::string> &origin){
+    system("clear");
+    bool out;
+    std::string data;
+    cout<<"\nType your data: ";
+    cin>>data;
+
+    out = origin.addNew(data);
+    return out;
+}
+//********************************************************************************
 int main(){
+    // Set up
     LinkedList<std::string> myList;
     std::string option;
-
     bool stat = true;
-    while(stat){
-        system("clear");
+    bool out = false;
 
-        cout<<"1. Add new element"<<endl;
-        cout<<"2. Add next to element"<<endl;
-        cout<<"3. Edit element"<<endl;
-        cout<<"4. Erase element"<<endl;
-        cout<<"5. Show content"<<endl;
-        cout<<"6. Exit"<<endl;
-        
-        cout<<"\n>>";
+    // LinkedList class implementation
+    while(stat){
+        options();        
+        cout<<"\n>> ";
         cin>>option;
 
         switch(atoi(option.c_str())){
             case 1:
+                out = add(myList);
                 break;
             case 2:
                 break;
@@ -31,19 +50,23 @@ int main(){
             case 4:
                 break;
             case 5:
+                cout<<endl;
+                myList.show();
                 break;
             case 6:
                 stat = false;
                 break;
             default:
-                cout<<"\nBad input"<<endl;
                 break;
-        }
-
-    cout<<"\nPress Enter to continue...";
+    }
+    
+    (out)?cout<<"\nSuccess\n":cout<<"\nFailed\n";
+    //cout<<"\nPress Enter to continue...\n";
     cin.ignore();
     cin.get();
     }
     
 return 0;
 }
+//********************************************************************************
+//********************************************************************************
